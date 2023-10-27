bfddaad727fada693ea96e71743bdc9247cad909
A1exFox <oxoma91@gmail.com>
2023-10-27 21:15:41 +0300

    feat: move tile up

--- js/script.js
+++ js/script.js
@@ -11,33 +11,60 @@ setupInputOnce();
 function setupInputOnce() {
   window.addEventListener('keydown', handleInput, { once: true });
 }
-function handleInput(event) {
+async function handleInput(event) {
   switch (event.key) {
     case 'ArrowUp':
-      moveUp();
+      // console.log(canMoveUp());
+      // if (!canMoveUp()) {
+      //   setupInputOnce();
+      //   return;
+      // }
+      await moveUp();
       break;
     case 'ArrowDown':
+      await moveDown()
       break;
     case 'ArrowLeft':
+      await moveLeft();
       break;
     case 'ArrowRight':
+      await moveRight()
       break;
 
     default:
       setupInputOnce();
       return;
   }
+
+  const newTile = new Tile(gameBoard);
+  grid.getRandomEmptyCell().linkTile(newTile);
+
   setupInputOnce();
 }
 
-function moveUp() {
-  slideTiles(grid.cellsGroupedByColumn);
+async function moveUp() {
+  await slideTiles(grid.cellsGroupedByColumn);
 }
-function slideTiles(groupedCells) {
-  console.log(groupedCells)
-  groupedCells.forEach(group => slideTilesInGroup(group));
+async function moveDown() {
+  await slideTiles(grid.cellsGroupedByReversedColumn);
 }
-function slideTilesInGroup(group) {
+async function moveLeft() {
+  await slideTiles(grid.cellsGroupedByRow);
+}
+async function moveRight() {
+  await slideTiles(grid.cellsGroupedByReversedRow);
+}
+async function slideTiles(groupedCells) {
+  const promises = []
+  groupedCells.forEach(group => slideTilesInGroup(group, promises));
+
+  await Promise.all(promises);
+
+  grid.cells.forEach(cell => {
+    cell.hasTileForMerge() && cell.mergeTiles();
+  })
+}
+function slideTilesInGroup(group, promises) {
   for (let i = 1; i < group.length; i++) {
     if (group[i].isEmpty()) continue;
     const cellWithTile = group[i];
@@ -50,6 +77,9 @@ function slideTilesInGroup(group) {
     }
 
     if (!targetCell) continue;
+
+    promises.push(cellWithTile.linkedTile.waitForTransitionEnd())
+
     if (targetCell.isEmpty()) {
       targetCell.linkTile(cellWithTile.linkedTile)
     } else {
@@ -58,4 +88,20 @@ function slideTilesInGroup(group) {
 
     cellWithTile.unlinkTile();
   }
+}
+
+function canMoveUp() {
+  return canMove(grid.cellsGroupedByColumn);
+}
+function canMove(groupedCells) {
+  return groupedCells.some(group => canMoveInGroup(group));
+}
+
+function canMoveInGroup(group) {
+  group.some((cell, index) => {
+    if (index === 0) return false;
+    if (cell.isEmpty()) return false;
+    const targetCell = group[index - 1];
+    return targetCell.canAccept(cell.linkedTile);
+  });
 }
